9d81a3273e5c19ace5238958307bb27d1e594532
Knox316 <fernandocorreia316@gmail.com>
2019-06-12 10:39:31 +0100

    jwt working

---
+++ maquetes/src/components/UserForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class UserForm extends Component {
+  state = {
+    email: "",
+    password: ""
+  };
+  render() {
+    return <div />;
+  }
+}
+
+export default UserForm;
