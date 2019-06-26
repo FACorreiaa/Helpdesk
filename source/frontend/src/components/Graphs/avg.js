7bf3346925a855e6bd0580baecad25500430b24e
Knox316 <fernandocorreia316@gmail.com>
2019-06-26 22:49:47 +0100

    versao atualizada

--- source/frontend/src/components/Graphs/avg.js
+++ source/frontend/src/components/Graphs/avg.js
@@ -12,35 +12,36 @@ class Avg extends Component {
     super(props);
     this.state = {
       formFields: { from: "", to: "" },
-      score: 1,
-      finalDate: ["2019-06-01"]
-    };
+      //score: 1,
+      //finalDate: ["2019-06-01"]
+      labels: [],
+      data: []
 
-    const data = {
-      labels: this.state.finalDate,
-      datasets: [
-        {
-          label: "Score",
-          fill: false,
-          lineTension: 0.1,
-          backgroundColor: "rgba(75,192,192,0.4)",
-          borderColor: "rgba(75,192,192,1)",
-          borderCapStyle: "butt",
-          borderDash: [],
-          borderDashOffset: 0.0,
-          borderJoinStyle: "miter",
-          pointBorderColor: "rgba(75,192,192,1)",
-          pointBackgroundColor: "#fff",
-          pointBorderWidth: 1,
-          pointHoverRadius: 5,
-          pointHoverBackgroundColor: "rgba(75,192,192,1)",
-          pointHoverBorderColor: "rgba(220,220,220,1)",
-          pointHoverBorderWidth: 2,
-          pointRadius: 1,
-          pointHitRadius: 10,
-          data: this.state.score
-        }
-      ]
+      // const data = {
+      //   labels: this.state.finalDate,
+      //   datasets: [
+      //     {
+      //       label: "Score",
+      //       fill: false,
+      //       lineTension: 0.1,
+      //       backgroundColor: "rgba(75,192,192,0.4)",
+      //       borderColor: "rgba(75,192,192,1)",
+      //       borderCapStyle: "butt",
+      //       borderDash: [],
+      //       borderDashOffset: 0.0,
+      //       borderJoinStyle: "miter",
+      //       pointBorderColor: "rgba(75,192,192,1)",
+      //       pointBackgroundColor: "#fff",
+      //       pointBorderWidth: 1,
+      //       pointHoverRadius: 5,
+      //       pointHoverBackgroundColor: "rgba(75,192,192,1)",
+      //       pointHoverBorderColor: "rgba(220,220,220,1)",
+      //       pointHoverBorderWidth: 2,
+      //       pointRadius: 1,
+      //       pointHitRadius: 10,
+      //       data: this.state.score
+      //     }
+      //   ]
     };
   }
 
@@ -93,9 +94,8 @@ class Avg extends Component {
             </Button>{" "}
           </Grid>{" "}
         </form>
-        <h2>Média</h2>
 
-        <Line data={this.data} />
+        <Line data={this.state} />
       </div>
     );
   }
@@ -117,8 +117,30 @@ class Avg extends Component {
       console.log(score);
       console.log(finalDate);
       this.setState({
-        score: score,
-        dates: finalDate
+        labels: finalDate,
+        datasets: [
+          {
+            label: "média",
+            fill: true,
+            lineTension: 0.1,
+            backgroundColor: "rgba(75,192,192,0.4)",
+            borderColor: "rgba(75,192,192,1)",
+            borderCapStyle: "butt",
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: "miter",
+            pointBorderColor: "rgba(75,192,192,1)",
+            pointBackgroundColor: "#fff",
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: "rgba(75,192,192,1)",
+            pointHoverBorderColor: "rgba(220,220,220,1)",
+            pointHoverBorderWidth: 2,
+            pointRadius: 0.1,
+            pointHitRadius: 10,
+            data: score
+          }
+        ]
       });
     } catch (error) {
       console.log(error);
